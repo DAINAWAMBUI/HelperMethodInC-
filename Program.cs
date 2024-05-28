fb8471df3bf13787cb69fbf27394f50aa1a9733c
Daina Muiruri <61407935+DAINAWAMBUI@users.noreply.github.com>
2024-05-28 10:20:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace HelperMethods
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            //string myValue = superSecretFormula("world");
+            string myValue = superSecretFormula("My World");
+            Console.WriteLine(myValue);
+            Console.ReadLine();
+        }
+        private static string superSecretFormula()
+        {
+            return ("Hello World");
+        }
+        private static string superSecretFormula(string name)
+        {
+            return string.Format("Hello, {0}!" , name);
+        }
+    }
+}
